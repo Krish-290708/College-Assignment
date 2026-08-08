d24c35ddae3f16daa414b706c7da9a65691c9820
Krish-290708 <krishpp.290708@outlook.com>
2026-08-08 15:18:28 +0530

    git.commit

---
+++ Practical Codes/Practical_PracticeSession.py
@@ -0,0 +1,17 @@
+#lst=[]
+#print(type(lst))
+
+#names=["Priya","Jack","Jacob",1,2,3,4,5]
+#print(names)
+
+#Calculating the circumference of a circle
+import math
+'''radius = float(input("Enter the radius of the circle = "))
+circumference = 2 * math.pi * radius
+print(f"The circumference of the circle is {round(circumference, 3)}cm")'''
+
+#Calculating the area of a circle
+radius = float(input("Enter the radius of the circle = "))
+area = math.pi * pow(radius,2)
+print(f"The area of the circle is {round(area, 3)}cm^2")
+
---
+++ Theory Codes/Data_Structures.py
@@ -0,0 +1,13 @@
+## creating a tuple
+'''empty_tuple=()
+print(empty_tuple)
+print(type(empty_tuple))
+
+lst=list()
+print(type(lst))
+tpl=tuple()
+print(type(tpl))'''
+
+#numbers=tuple([1,2,3,4,5,6])
+#print(numbers) 
+
