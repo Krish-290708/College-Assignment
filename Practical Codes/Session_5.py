cbe99f98f1fafac90dde573efe21cfb4a0591ea7
Krish-290708 <krishpp.290708@outlook.com>
2026-08-25 08:45:25 +0530

    Session 5

--- Practical Codes/Session_5.py
+++ Practical Codes/Session_5.py
@@ -1,15 +1,38 @@
-#EXERCISE
+'''username = input("Enter your User ID: ")
+password = input("Enter your Password: ")
+if username == "krish@nmims":
+    print("Valid Username")
+else:
+    print("Invalid")
+if password.isdigit():
+    print("Valid Password")
+
+    sap_id = input("Enter your SAP ID:")
+    roll_no = input("Enter your Roll no.:")
+    course = input("Enter your Course name:")
+
+    print("Student Information:")
+    print(f"SAP ID: {sap_id}")
+    print(f"Roll no.:{roll_no}")
+    print(f"Course:{course}")
+else:
+    print("Invalid User ID or Password")'''
+
+#Exercise
 #1.
-item = input("what item/s have you bought?: ")
-quantity = int(input("How many have you bought?: "))
-price = float(input("What is the price of the item?: "))
-total = price * quantity
-print(f"Total = {total} ")
+items = input("What is the item purchased:")
+quantity = int(input("What is the quantity of the item purchased:"))
+price = float(input("What is the price of the item:"))
+total = price*quantity
+
+print(f"{'items':<10}{'quantity':>10}{'price':>10}{'total':>10}")
+print("-"*45)
+print(f"{items:<10}{quantity:>10}{price:>10.2f}{total:>10.2f}")
 
 #2.
-num = int(input("Enter a number: "))
+'''num = int(input("Enter a number:"))
 
 if num % 3 == 0 and num % 5 == 0:
-    print(num, "is divisible by both 3 and 5")
+    print("The number is divisible by both 3 and 5.")
 else:
-    print(num, "is not divisible by both 3 and 5")
+    print("The number is not divisible by either 3 or 5 or both.")'''
